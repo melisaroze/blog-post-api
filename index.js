f99fedd629039ea22b3f4372d3a7be4f41aa91b5
melizabelgica <melizabelgica@gmail.com>
2025-10-21 18:51:13 +0800

    update cors option

--- index.js
+++ index.js
@@ -4,27 +4,30 @@ const cors = require("cors");
 
 const port = 4000;
 
+require("dotenv").config();
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.use(cors({
-  origin: [
+
+const corsOptions = {
+    origin: [
     "http://localhost:3000",             
     "http://localhost:4000",             
     "https://blog-post-api-alvarez.onrender.com" ,
     "https://blog-post-client-alvarez.vercel.app" 
-  ],
-  methods: ["GET", "POST", "PATCH", "DELETE"],
-  credentials: true
-}));
+    ],
+    credentials: true,
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
 
 //MongoDB database
-mongoose.connect("mongodb+srv://admin:admin@b561-alvarez.gh8mqev.mongodb.net/BlogPostAPI?retryWrites=true&w=majority&appName=B561-Alvarez", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGODB_STRING);
 
 mongoose.connection.once('open', () => console.log('Now connected to MongoDB Atlas.'));
 
